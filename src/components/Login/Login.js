84bdb27386acf52084fe119552929097887af8af
Fattakhova <ilnuro4ka01@gmail.com>
2021-09-08 01:29:31 +0500

    added new css files

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -2,7 +2,7 @@ function Login() {
   return (
     <>
         <section className='login'>
-          <img className='login__icon' />
+          {/* <img className='login__icon' /> */}
           <h3 className='login__title'>Рады видеть</h3>
           <form className='login__form'>
             <label for='email'>E-mail</label>
@@ -11,7 +11,7 @@ function Login() {
             <input id='password' type='password' />
             <button type='submit'>Войти</button>
           </form>
-          <p>Ещё не зарегестрированы? <a href='#'>Регистрация</a></p>
+          <p>Ещё не зарегестрированы? <a href='asd'>Регистрация</a></p>
         </section>
     </>
   )
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -2,7 +2,7 @@ function Register() {
   return (
     <>
         <section className='register'>
-          <img className='register__icon' />
+          {/* <img className='register__icon' /> */}
           <h3 className='register__title'>Добро пожаловать</h3>
           <form className='register__form'>
             <label for='name'>Имя</label>
