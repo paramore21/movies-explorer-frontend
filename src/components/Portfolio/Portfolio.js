4261e4f4382906a2e3f2c580cace83c356d59615
Fattakhova <ilnuro4ka01@gmail.com>
2021-09-20 01:26:11 +0500

    320 resolution finished

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -7,7 +7,7 @@ function Portfolio(){
         <li className='portfolio__link-item'>
           <a href='#' target='_blank' className='portfolio__link'>
             <p className='portfolio__link-text'>Статичный сайт</p>
-            <svg className='portfolio__link-icon' width="18" height="17" viewBox="0 0 18 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <svg className='portfolio__link-icon' viewBox="0 0 18 17" fill="none" xmlns="http://www.w3.org/2000/svg">
               <path d="M2.60653 16.5241L14.9645 4.14489L14.9432 13.6903H17.2656V0.181818H3.77841L3.7571 2.48295H13.3026L0.944603 14.8622L2.60653 16.5241Z" fill="black"/>
             </svg>
           </a>
@@ -15,7 +15,7 @@ function Portfolio(){
         <li className='portfolio__link-item'>
           <a href='#' target='_blank' className='portfolio__link'>
             <p className='portfolio__link-text'>Адаптивный сайт</p>
-            <svg className='portfolio__link-icon' width="18" height="17" viewBox="0 0 18 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <svg className='portfolio__link-icon' viewBox="0 0 18 17" fill="none" xmlns="http://www.w3.org/2000/svg">
               <path d="M2.60653 16.5241L14.9645 4.14489L14.9432 13.6903H17.2656V0.181818H3.77841L3.7571 2.48295H13.3026L0.944603 14.8622L2.60653 16.5241Z" fill="black"/>
             </svg>
           </a>
@@ -23,7 +23,7 @@ function Portfolio(){
         <li className='portfolio__link-item'>
           <a href='#' target='_blank' className='portfolio__link'>
             <p className='portfolio__link-text'>Одностраничное приложение</p>
-            <svg className='portfolio__link-icon' width="18" height="17" viewBox="0 0 18 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <svg className='portfolio__link-icon' viewBox="0 0 18 17" fill="none" xmlns="http://www.w3.org/2000/svg">
               <path d="M2.60653 16.5241L14.9645 4.14489L14.9432 13.6903H17.2656V0.181818H3.77841L3.7571 2.48295H13.3026L0.944603 14.8622L2.60653 16.5241Z" fill="black"/>
             </svg>
           </a>
