afd8a102c12c6859d96cab6b5d74cc755958ec3e
Fattakhova <ilnuro4ka01@gmail.com>
2021-09-23 17:47:33 +0500

    movie card started

--- src/components/MoviesCard/MoviesCard.js
+++ src/components/MoviesCard/MoviesCard.js
@@ -1,9 +1,11 @@
+import movie1 from '../../images/movie1.png'
 function MoviesCard(){
   return (
     <section className='movies-card'>
       <h3 className='movies-card__title'>В погоне за Бенкси</h3>
       <p className='movies-card__duration'>27 минут</p>
-      
+      <img className='movies-card__image' src={movie1} alt='Постер фильма' />
+      <button className='movies-card__button'>Сохранить</button>
     </section>
   )
 }
