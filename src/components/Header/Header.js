6bcd1a05dd09ca51ae00c40cc7285488d3b0cd90
Fattakhova <ilnuro4ka01@gmail.com>
2021-10-05 02:16:16 +0500

    header light added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,14 @@
 import icon from '../../images/logo.png';
+import {useLocation} from "react-router-dom";
 function Header() {
+  const {path} = useLocation()
+  const buttonText = `${path === '/' ? 'Войти' : 'Аккаунт'}`
   return (
-    <section className='header'>
+    <section className={`${path === '/' ? 'header' : 'header header_light'}`}>
       <a href={'/'} className='header__link'><img className='header__icon' alt='Логотип' src={icon} /></a>
       <div className='header__buttons'>
-        <a href={'/signup'} className='header__register-button'>Регистрация</a>
-        <a href={'/signin'} className='header__login-button'>Войти</a>
+        <a href={'/signup'} className={`${path === '/' ? 'header__register-button' : 'header__register-button header__register-button_light'}`}>Регистрация</a>
+        <a href={'/signin'} className={`${path === '/' ? 'header__login-button' : 'header__login-button header__login-button_light'}`}>{buttonText}</a>
       </div>
     </section>
   )
