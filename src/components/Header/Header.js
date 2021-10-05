16c7fd9e78fe867e508639ff28eecb974bcf1888
Fattakhova <ilnuro4ka01@gmail.com>
2021-10-05 18:14:28 +0500

    account icon edited

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,14 @@
 import icon from '../../images/logo.png';
 import {useLocation} from "react-router-dom";
 function Header() {
-  const {path} = useLocation()
-  const buttonText = `${path === '/' ? 'Войти' : 'Аккаунт'}`
+  const { pathname } = useLocation()
+  const buttonText = `${pathname === '/' ? 'Войти' : 'Аккаунт'}`
   return (
-    <section className={`${path === '/' ? 'header' : 'header header_light'}`}>
+    <section className={`${pathname === '/' ? 'header' : 'header header_light'}`}>
       <a href={'/'} className='header__link'><img className='header__icon' alt='Логотип' src={icon} /></a>
       <div className='header__buttons'>
-        <a href={'/signup'} className={`${path === '/' ? 'header__register-button' : 'header__register-button header__register-button_light'}`}>Регистрация</a>
-        <a href={'/signin'} className={`${path === '/' ? 'header__login-button' : 'header__login-button header__login-button_light'}`}>{buttonText}</a>
+        <a href={'/signup'} className={`${pathname === '/' ? 'header__register-button' : 'header__register-button header__register-button_light'}`}>Регистрация</a>
+        <a href={'/signin'} className={`${pathname === '/' ? 'header__login-button' : 'header__login-button header__login-button_light'}`}>{buttonText}</a>
       </div>
     </section>
   )
