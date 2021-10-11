efb64d058da9617f0769096824dc6f5737edf4eb
Fattakhova <ilnuro4ka01@gmail.com>
2021-10-11 18:01:00 +0500

    burger menu some fixes

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,6 @@
 import icon from '../../images/logo.png';
+import close_icon from '../../images/close-icon.svg';
 import burger_icon from '../../images/burger-icon.svg';
-import close_icon from '../../images/cancel-icon.svg';
 import {useLocation} from "react-router-dom";
 import {useState} from "react";
 function Header() {
@@ -21,7 +21,7 @@ function Header() {
         </div>
         <div className='header__buttons'>
           <a href={'/signup'} className={`${pathname === '/' ? 'header__register-button' : 'header__register-button header__register-button_light'}`}>Регистрация</a>
-          <a href={'/signin'} className={`${pathname === '/' ? 'header__login-button' : 'header__login-button header__login-button_light'}`}>{buttonText}</a>
+          <a href={pathname === '/'? '/signin' : '/profile'} className={`${pathname === '/' ? 'header__login-button' : 'header__login-button header__login-button_light'}`}>{buttonText}</a>
         </div>
       </section>
       <section className='header__burger'>
@@ -29,11 +29,13 @@ function Header() {
         <p className='header__link' onClick={handleMenuOpen}><img className='header__icon' alt='Логотип' src={burger_icon} /></p>
         <div className={isMenuOpen ? "header__burger-menu header__burger-menu_opened" : "header__burger-menu"}>
           <ul className="header__burger-box">
-            <li className='header__burger-item' onClick={handleMenuOpen}><img src={close_icon} alt='Закрыть' className="header__close-icon"/></li>
-            <li className='header__burger-item'><a className="header__burger-link" href={'/'}>Главная</a></li>
-            <li className='header__burger-item'><a className="header__burger-link" href={'/movies'}>Фильмы</a></li>
-            <li className='header__burger-item'><a className="header__burger-link" href={'/saved-movies'}>Сохраненные фильмы</a></li>
-            <li className='header__burger-item'><a href={'/signin'} className='header__login-button header__login-button_light'>Аккаунт</a></li>
+            <li className='header__burger-close-icon' onClick={handleMenuOpen}><img src={close_icon} alt='Закрыть' className="header__close-icon"/></li>
+            <ul className='header__burger-sub-box'>
+              <li className='header__burger-item'><a className={ pathname === '/' ? "header__burger-link header__burger-link_active" : "header__burger-link"} href={'/'}>Главная</a></li>
+              <li className='header__burger-item'><a className={ pathname === '/movies' ? "header__burger-link header__burger-link_active" : "header__burger-link"} href={'/movies'}>Фильмы</a></li>
+              <li className='header__burger-item'><a className={ pathname === '/saved-movies' ? "header__burger-link header__burger-link_active" : "header__burger-link"} href={'/saved-movies'}>Сохраненные фильмы</a></li>
+              <li className='header__burger-item'><a href={'/profile'} className='header__burger-account-button'>Аккаунт</a></li>
+            </ul>
           </ul>
         </div>
       </section>
